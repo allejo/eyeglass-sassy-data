63c1b48b8236a2799ca305cc21114deec440f255
allejo <vladimir.jimenez.102@my.csun.edu>
2016-11-17 18:23:26 -0800

    Add basic functions that work

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -2,14 +2,16 @@
 
 var fs   = require('fs');
 var eol  = require('eol');
-var sass = require('node-sass');
+var path = require('path');
 var yaml = require('js-yaml');
-var sassUtils = require('node-sass-utils')(sass);
 
 module.exports = function(eyeglass, sass) {
+    var sassUtils = require('node-sass-utils')(sass);
+
     return {
+        sassDir: path.join(__dirname, '..', 'sass'),
         functions: {
-            "import-from-json($filePath, $encoding: 'utf8')": function($filePath, $encoding, done) {
+            "sd-import-from-json($filePath, $encoding: 'utf8')": function($filePath, $encoding, done) {
                 fs.readFile($filePath.getValue(), $encoding.getValue(), function (err, data) {
                     if (err) {
                         done(sass.types.Error("File read error: \'" + err.toString() + "\'."));
@@ -17,7 +19,26 @@ module.exports = function(eyeglass, sass) {
                     }
 
                     var pData = JSON.parse(eol.auto(data));
-                    done(sassUtils.castToSass(sassUtils.castToSass(pData)));
+                    done(sassUtils.castToSass(pData));
+                });
+            },
+            "sd-read-json($json)": function ($json, done) {
+                try {
+                    var data = JSON.parse($json.getValue());
+                    done(sassUtils.castToSass(data));
+                } catch (e) {
+                    done(sass.types.Error("JSON parsing error: " + e));
+                }
+            },
+            "sd-import-from-yaml($filePath, $encoding: 'utf8')": function ($filePath, $encoding, done) {
+                fs.readFile($filePath.getValue(), $encoding.getValue(), function (err, data) {
+                    if (err) {
+                        done(sass.types.Error("File read error: \'" + err.toString() + "\'."));
+                        return;
+                    }
+
+                    var pData = yaml.safeLoad(eol.auto(data));
+                    done(sassUtils.castToSass(pData));
                 });
             }
         }
