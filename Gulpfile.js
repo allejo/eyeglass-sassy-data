cfb6359130ae38a763a81c10488db265decb62cc
allejo <allejo@me.com>
2016-11-29 15:14:00 -0800

    Attempt to have unit tests

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var path = require('path');
 var pump = require('pump');
 
 gulp.task('sass:docs', function (cb) {
@@ -7,5 +8,24 @@ gulp.task('sass:docs', function (cb) {
     pump([
         gulp.src('sass/*.scss'),
         sassdoc()
+    ], cb);
+});
+
+gulp.task('sass:tests', function (cb) {
+    var sass = require('gulp-sass');
+    var eyeglass = require('eyeglass');
+
+    pump([
+        gulp.src('tests/tests.scss'),
+        sass(eyeglass({
+            includePaths: 'node_modules/sass-true/sass/',
+            eyeglass: {
+                modules: [
+                    {
+                        path: path.join(__dirname)
+                    }
+                ]
+            }
+        }))
     ], cb);
 });
